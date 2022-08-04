c68c2ce7f177243f0b7e9349418a20a2bdccb2b0
gapigo <x.gabriel12@gmail.com>
2022-08-04 11:30:28 -0300

    Inclusão do Room e funcionaldiades de "Share" e "Favorite"

--- app/src/main/java/me/dio/soccernews/ui/adapter/NewsAdapter.java
+++ app/src/main/java/me/dio/soccernews/ui/adapter/NewsAdapter.java
@@ -1,5 +1,6 @@
 package me.dio.soccernews.ui.adapter;
 
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.view.LayoutInflater;
@@ -12,15 +13,18 @@ import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
+import me.dio.soccernews.R;
 import me.dio.soccernews.databinding.NewsItemBinding;
 import me.dio.soccernews.domain.News;
 
 public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.ViewHolder> {
 
-    private List<News> news;
+    private final List<News> news;
+    private final FavoriteListener favoriteListener;
 
-    public NewsAdapter(List<News> news) {
+    public NewsAdapter(List<News> news, FavoriteListener favoriteListener) {
         this.news = news;
+        this.favoriteListener = favoriteListener;
     }
 
     @NonNull
@@ -33,15 +37,37 @@ public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.ViewHolder> {
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Context context = holder.itemView.getContext();
         News news = this.news.get(position);
-        holder.binding.tvTitle.setText(news.getTitle());
-        holder.binding.tvDescription.setText(news.getDescription());
-        Picasso.get().load(news.getImage()).into(holder.binding.ivThumbnail);
+        holder.binding.tvTitle.setText(news.title);
+        holder.binding.tvDescription.setText(news.description);
+        Picasso.get().load(news.image).into(holder.binding.ivThumbnail);
+
+        // Funcionalidade de "Abrir Link"
+
         holder.binding.btOpenLink.setOnClickListener(view -> {
             Intent i = new Intent(Intent.ACTION_VIEW);
-            i.setData(Uri.parse(news.getLink()));
-            holder.itemView.getContext().startActivity(i);
+            i.setData(Uri.parse(news.link));
+            context.startActivity(i);
         });
+
+        // Implementação de funcionalidade "compartilhar"
+        holder.binding.ivShare.setOnClickListener(view -> {
+            Intent intent = new Intent(Intent.ACTION_SEND);
+            intent.setType("text/plain");
+            intent.putExtra(Intent.EXTRA_SUBJECT, news.title);
+            intent.putExtra(Intent.EXTRA_TEXT, news.link);
+            context.startActivity(Intent.createChooser(intent, "Share via"));
+        });
+
+        // Implementação da funcionalidade de "Favoritar" (o evento será instanciado pelo Fragment)
+        holder.binding.ivFavorite.setOnClickListener(view -> {
+            news.favorite = !news.favorite;
+            this.favoriteListener.onFavorite(news);
+            notifyItemChanged(position);
+        });
+        int favoriteColor = news.favorite ? R.color.favorite_active : R.color.favorite_default;
+        holder.binding.ivFavorite.setColorFilter(context.getResources().getColor(favoriteColor));
     }
 
     @Override
@@ -56,5 +82,9 @@ public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.ViewHolder> {
             super(binding.getRoot());
             this.binding = binding;
         }
+    }
+
+    public interface FavoriteListener {
+        void onFavorite(News news);
     }
 }
--- app/src/main/java/me/dio/soccernews/ui/news/NewsViewModel.java
+++ app/src/main/java/me/dio/soccernews/ui/news/NewsViewModel.java
@@ -1,12 +1,16 @@
 package me.dio.soccernews.ui.news;
 
+import android.app.Application;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
+import androidx.room.Room;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import me.dio.soccernews.data.local.AppDatabase;
 import me.dio.soccernews.data.remote.SoccerNewsApi;
 import me.dio.soccernews.domain.News;
 import retrofit2.Call;
@@ -17,7 +21,12 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class NewsViewModel extends ViewModel {
 
+    public enum State {
+        DOING, DONE, ERROR;
+    }
+
     private final MutableLiveData<List<News>> news = new MutableLiveData<>();
+    private final MutableLiveData<State> state = new MutableLiveData<>();
     private final SoccerNewsApi api;
 
     public NewsViewModel() {
@@ -30,19 +39,21 @@ public class NewsViewModel extends ViewModel {
     }
 
     private void findNews() {
+        state.setValue(State.DOING);
         api.getNews().enqueue(new Callback<List<News>>() {
             @Override
             public void onResponse(Call<List<News>> call, Response<List<News>> response) {
                 if (response.isSuccessful()) {
                     news.setValue(response.body());
+                    state.setValue(State.DONE);
                 } else {
-                    //TODO Pensar em uma estratégia de tratamento de erros.
+                    state.setValue(State.ERROR);
                 }
             }
 
             @Override
             public void onFailure(Call<List<News>> call, Throwable t) {
-                //TODO Pensar em uma estratégia de tratamento de erros.
+                state.setValue(State.ERROR);
             }
         });
     }
@@ -50,4 +61,5 @@ public class NewsViewModel extends ViewModel {
     public LiveData<List<News>> getNews() {
         return this.news;
     }
+    public LiveData<State> getState() {return this.state;}
 }
